a8a6d9be1f13520d3b4bba071ebbba9bfdc818c0
marius <marius@marius-dev.(none)>
2011-11-20 10:44:01 +0200

    replacing everything

--- demo/project/build/DemoProject.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-class DemoProject(info: ProjectInfo) extends DefaultWebProject(info)  {
-
-  val jetty7 = "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test"
-
-}
--- project/build/ShiftParent.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-class ShiftParent(info: ProjectInfo) extends ParentProject(info) {
-
-   lazy val engine = project("engine")
-
-   lazy val demo = project("demo", engine)
-}
