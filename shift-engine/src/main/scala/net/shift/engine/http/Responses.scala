77bae417246271e84e37b4f6de8c1ea1ee0695d4
Marius Danciu <marius.danciu@gmail.com>
2013-09-08 16:51:16 +0300

    more

--- shift-engine/src/main/scala/net/shift/engine/http/Responses.scala
+++ shift-engine/src/main/scala/net/shift/engine/http/Responses.scala
@@ -6,7 +6,6 @@ import io.WriteChannel
 import scala.xml._
 import common._
 
-
 case class TextResponse(text: String) extends Response {
   def code = 200
   def reason = "OK"
@@ -14,16 +13,39 @@ case class TextResponse(text: String) extends Response {
   def contentType = Some("text/plain; charset=\"UTF-8\"")
   def cookies = Nil
   def writeBody(channel: WriteChannel) = {
-    channel.writeBuffer(text getBytes("UTF-8"))
+    channel.writeBuffer(text getBytes ("UTF-8"))
   }
 }
-case class Html5Response(content : NodeSeq) extends Response {
+
+case class Html5Response(content: NodeSeq) extends Response {
   def code = 200
   def reason = "OK"
   def headers = Map.empty
   def contentType = Some("text/html; charset=\"UTF-8\"")
   def cookies = Nil
   def writeBody(channel: WriteChannel) = {
-    channel.writeBuffer(("<!DOCTYPE html>\n"+XmlUtils.mkString(content)).getBytes("UTF-8"))
+    channel.writeBuffer(("<!DOCTYPE html>\n" + XmlUtils.mkString(content)).getBytes("UTF-8"))
   }
 }
+
+ case class CSSResponse(content: String) extends Response {
+   def code = 200
+   def reason = "OK"
+   def headers = Map.empty
+   def contentType = Some("text/css; charset=\"UTF-8\"")
+   def cookies = Nil
+   def writeBody(channel: WriteChannel) = {
+     channel.writeBuffer(content.getBytes("UTF-8"))
+   }
+ }
+
+ case class JSResponse(content: String) extends Response {
+   def code = 200
+   def reason = "OK"
+   def headers = Map.empty
+   def contentType = Some("text/javascript; charset=\"UTF-8\"")
+   def cookies = Nil
+   def writeBody(channel: WriteChannel) = {
+     channel.writeBuffer(content.getBytes("UTF-8"))
+   }
+ }
